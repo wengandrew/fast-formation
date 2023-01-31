520991db6d39196d0c254d0d8d1f9735ed93cf2f
Andrew Weng <weng.andrew@gmail.com>
2023-01-31 16:39:45 +0000

    WIP

---
+++ src/parsers.py
@@ -0,0 +1,30 @@
+"""
+A collection of utility classes and methods for parsing raw timeseries data.
+
+Written to handle specifically data from the project 'UMBL2022FEB'.
+
+Andrew Weng
+2023/01/31
+"""
+
+
+class RptParser:
+    
+    def __init__():
+        
+        pass
+    
+    
+class CyclingParser:
+    
+    def __init__():
+        
+        pass
+    
+    
+class FormationParser:
+    
+    
+    def __init__():
+        
+        pass
